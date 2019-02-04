a70423c962663b162a1ee5e19d97925979f760fa
Robert L. Kreft <kreftrl@rose-hulman.edu>
2019-02-04 13:09:26 -0500

    commit

--- src/m2_tkinter_as_mqtt_sender.py
+++ src/m2_tkinter_as_mqtt_sender.py
@@ -2,7 +2,7 @@
 Using a fake robot as the receiver of messages.
 """
 
-# TODO: 1. In mqtt_remote_method_calls, set LEGO_NUMBER at line 131
+# DONE: 1. In mqtt_remote_method_calls, set LEGO_NUMBER at line 131
 # to YOUR robot's number.
 
 # TODO: 2. Copy your Tkinter/ttk ROBOT gui code from the previous session (m6).
